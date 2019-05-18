055ab82c7ebb16761beca3fba15f7f60f2b7ebd2
Stanley Louis <stanlouis@msn.com>
2019-05-18 13:20:44 -0400

    Initialize repository

---
+++ src/components/Die.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Die extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Roll it</h1>
+      </div>
+    )
+  }
+}
